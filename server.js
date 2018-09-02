360e7148a782234855c6890c5b6b877c002a8b02
Grégoire Sage <gregoire.sage@gmail.com>
2018-09-03 00:16:23 +0200

    fix port for heroku

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get('/image', (request, response) => {
   savePixels(image, 'png').pipe(response)
 })
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000')
 })
